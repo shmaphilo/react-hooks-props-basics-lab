c2af5f28807d52629394daa5e9f77573f1e624da
shmaphilo <philipogaye062@gmail.com>
2023-07-22 23:40:30 -0400

    assignment done

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Links from "./Link";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+       {props.bio}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+       {<Links />}
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Link";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
@@ -11,9 +12,11 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name="Liza" city="New York" color="firebrick" />
+      <About links={user.links} />
+      
     </div>
+      
   );
 }
 
